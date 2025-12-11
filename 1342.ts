ae5d2918a9859007e6660d1167574b00dd7300d3
spaceman1412 <spacemannormal@gmail.com>
2025-12-11 16:26:04 +0700

    Implement solution for "Number of Steps to Reduce a Number to Zero" with step counting logic and example usage

--- 1342.ts
+++ 1342.ts
@@ -1,3 +1,22 @@
 // 1342. Number of Steps to Reduce a Number to Zero
 
-function numberOfSteps(num: number): number {}
+function numberOfSteps(num: number): number {
+    // Ok quite simple we reduce num to 0, even we divided by 2 and odd we subtract 1, and return the step numbers
+    let count = 0;
+
+    while (num != 0) {
+        if (num % 2 == 0) {
+            num = num / 2;
+        } else {
+            num = num - 1;
+        }
+        count++;
+    }
+
+    return count;
+}
+
+let num = 14;
+
+console.log(numberOfSteps(num));
+console.log(num);
