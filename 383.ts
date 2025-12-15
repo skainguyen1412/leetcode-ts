71e05b3c02dcc73d371d42fa7b6b006a2ffc26c6
spaceman1412 <spacemannormal@gmail.com>
2025-12-15 16:13:27 +0700

    Add hashmap implementation for "Ransom Note" problem to improve character counting logic

--- 383.ts
+++ 383.ts
@@ -19,3 +19,32 @@ function canConstruct(ransomNote: string, magazine: string): boolean {
 
     return true;
 }
+
+// Hashmap implement
+
+function canConstructHashMap(ransomNote: string, magazine: string): boolean {
+    const magazineMap: Map<string, number> = new Map();
+
+    // Ok let start implement hashmap
+
+    for (let i = 0; i < magazine.length; i++) {
+        let value = magazineMap.get(magazine[i]);
+
+        if (value) {
+            magazineMap.set(magazine[i], value + 1);
+        } else {
+            magazineMap.set(magazine[i], 1);
+        }
+    }
+
+    for (let j = 0; j < ransomNote.length; j++) {
+        let value = magazineMap.get(ransomNote[j]);
+        if (value > 0) {
+            magazineMap.set(ransomNote[j], value - 1);
+        } else {
+            return false;
+        }
+    }
+
+    return true;
+}
