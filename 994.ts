07d366dc3531872dc130416aba196989d9c1d3ad
spaceman1412 <spacemannormal@gmail.com>
2025-12-22 14:54:52 +0700

    Refactor solution for "Minimum Deletion Size" problem to include an optimized O(n) approach and clean up code structure

--- 994.ts
+++ 994.ts
@@ -1,27 +1,49 @@
 function minDeletionSize(strs: string[]): number {
     // O(n^2)
-    const strsColumn = [];
+    function o2(strs: string[]): number {
+        const strsColumn = [];
 
-    strs.map((str) => {
-        for(let i = 0; i < str.length; i++) {
-            if (strsColumn[i]) {
-                strsColumn[i] = strsColumn[i] + str[i];
-            } else {
-                strsColumn[i] = str[i];
+        strs.map((str) => {
+            for (let i = 0; i < str.length; i++) {
+                if (strsColumn[i]) {
+                    strsColumn[i] = strsColumn[i] + str[i];
+                } else {
+                    strsColumn[i] = str[i];
+                }
+            }
+        });
+
+        let countDelete = 0;
+
+        strsColumn.forEach((str, index) => {
+            for (let i = 0; i < str.length - 1; i++) {
+                if (str[i] > str[i + 1]) {
+                    countDelete++;
+                    break;
+                }
+            }
+        });
+
+        return countDelete;
+    }
+
+    // On solution
+
+    function on(strs: string[]): number {
+        // Need to find way to solve with on
+        let countDelete = 0;
+
+        for (let i = 0; i < strs[0].length; i++) {
+            for (let j = 0; j < strs.length - 1; j++) {
+                if (strs[j][i] > strs[j + 1][i]) {
+                    countDelete++;
+                    break;
+                }
             }
         }
-    })
 
-    let countDelete = 0;
+        return countDelete;
+    }
 
-    strsColumn.forEach((str,index) => {
-        for(let i = 0; i < str.length - 1; i++) {
-            if (str[i] > str[i+1]) {
-                countDelete++;
-                break;
-            } 
-        }
-    });
-
-    return countDelete;
-};
+    return on(strs);
+}
