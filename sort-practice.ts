78526b1e1ae23547507f8f422859d2e61bff659f
spaceman1412 <spacemannormal@gmail.com>
2026-05-28 11:25:48 +0700

    insertion sort complete 28/08

--- sort-practice.ts
+++ sort-practice.ts
@@ -15,7 +15,33 @@ class SortSolution {
 
         console.log(arr);
     }
+
+    insertionSort(arr: number[]) {
+        // pseudocode
+        // for i: 1 to length(A) - 1
+        //     j = i
+        //     while j > 0 and A[j-1] > A[j]
+        //         swap A[j] and A[j-1]
+        //         j = j - 1
+
+        // Ok i get it now
+        // Basically quite simple with each index increase we sort the part from beginning to that index
+
+        for (let i = 1; i < arr.length; i++) {
+            let j = i;
+
+            while (j > 0 && arr[j - 1] > arr[j]) {
+                let tempt = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = tempt;
+
+                j--;
+            }
+        }
+
+        console.log(arr);
+    }
 }
 
 const testSort = new SortSolution();
-testSort.bubbleSort([3, 1, 5, 4]);
+testSort.insertionSort([3, 1, 5, 4]);
