2289faaa02dd0322a941ea6ecb9564239531fa57
spaceman1412 <spacemannormal@gmail.com>
2026-06-01 13:26:08 +0700

    Finish factorial nubmer problem (quite confident about visualize recursive and backtracking now)

--- recursion.ts
+++ recursion.ts
@@ -50,15 +50,41 @@ class RecursionPractice {
         // Backtrack because the 1 execute first
         // Do this we dont have to make a sum parameter
 
-        if (current < 1) {
+        if (current == 0) {
             return 0;
         }
 
         let sum = current + this.sumFirstNForward(n, current - 1);
-        console.log(current);
+        return sum;
+    }
+
+    factorialNumber(x, count = 1) {
+        // Input:
+        //  X = 5
+        // Output:
+        //  120
+        // Explanation:
+        //  5! = 5*4*3*2*1
+        // -----------------
+        // Iterative approach
+        // let sum = 1;
+        // for (let i = 1; i <= x; i++) {
+        //     sum = sum * i;
+        // }
+        // return sum;
+        // -----------------
+        // Recursive (backtracking) approach
+        // Visualize it in your mind understand deeply
+
+        if (count > x) {
+            return 1;
+        }
+
+        const sum = count * this.factorialNumber(x, count + 1);
+
         return sum;
     }
 }
 
 const recursion = new RecursionPractice();
-console.log(recursion.sumFirstNForward(5));
+console.log(recursion.factorialNumber(5));
