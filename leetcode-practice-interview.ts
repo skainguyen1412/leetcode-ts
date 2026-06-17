47ce144b3b0b145c4184471872dd965e0d30d07f
spaceman1412 <spacemannormal@gmail.com>
2026-06-17 13:09:47 +0700

    feat: implement array rotation algorithm with O(1) extra space in Interview class

--- leetcode-practice-interview.ts
+++ leetcode-practice-interview.ts
@@ -1,1 +1,34 @@
-class Interview {}
+class Interview {
+    /**
+ Do not return anything, modify nums in-place instead.
+ */
+    rotate(nums: number[], k: number): void {
+        // normal way O(n) space;
+
+        // const defaultArr = [...nums];
+
+        // for (let i = 0; i < defaultArr.length; i++) {
+        //     let index = (i + k) % defaultArr.length;
+
+        //     nums[index] = defaultArr[i];
+        // }
+
+        function reverseElementIndex(start, end) {
+            while (start < end) {
+                let tempt = nums[start];
+                nums[start] = nums[end];
+                nums[end] = tempt;
+                start++;
+                end--;
+            }
+        }
+
+        nums.reverse();
+
+        k = k % nums.length;
+
+        reverseElementIndex(0, k - 1);
+        reverseElementIndex(k, nums.length - 1);
+        console.log(nums);
+    }
+}
